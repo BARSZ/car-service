35d21d8069273d155655652a34d399f350229d03
Vasil Simeonov <vasko3107@gmail.com>
2022-12-05 17:51:05 +0200

    relationship between autoservice and worker changed

--- src/main/java/com/project/cscb869/data/entity/Worker.java
+++ src/main/java/com/project/cscb869/data/entity/Worker.java
@@ -1,6 +1,9 @@
 package com.project.cscb869.data.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +12,10 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @Entity
+@Table(name = "worker")
 public class Worker extends BaseEntity{
     private ServiceType qualification;
+    @ManyToOne
+    @JoinColumn(name = "auto_service_id")
+    private AutoService autoService;
 }
